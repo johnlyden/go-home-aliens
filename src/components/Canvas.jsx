0d6607393c7f588d195a3da0c804b49759975f08
johnlyden <johnlydennc@gmail.com>
2018-02-25 12:16:04 -0600

    added functionality to decrement lives when a flyingObject makes it to earth - game resets when all lives lost

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -17,6 +17,15 @@ const Canvas = (props) => {
   const gameHeight = 1200;
   const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
 
+  const lives = [];
+  for (let i = 0; i < props.gameState.lives; i++) {
+    const heartPosition = {
+      x: -180 - (i * 70),
+      y: 35
+    };
+    lives.push(<Heart key={i} position={heartPosition}/>);
+  }
+
   return (
     <svg
       id="aliens-go-home-canvas"
@@ -62,6 +71,7 @@ const Canvas = (props) => {
           ))}
         </g>
       }
+      {lives}
     </svg>
   );
 };
--- src/reducers/moveObjects.js
+++ src/reducers/moveObjects.js
@@ -21,6 +21,20 @@ function moveObjects(state, action) {
     (now - object.createdAt) < 4000
   ));
 
+  // flyingObject hit the earth and we lost a life
+  const lostLife = state.gameState.flyingObjects.length > flyingObjects.length;
+  let lives = state.gameState.lives;
+  if (lostLife) {
+    lives--;
+  }
+
+  const started = lives > 0;
+  if (!started) {
+    flyingObjects = [];
+    cannonBalls = [];
+    lives = 3;
+  }
+
   const { x, y } = mousePosition;
   const angle = calculateAngle(0, 0, x, y);
 
@@ -36,7 +50,9 @@ function moveObjects(state, action) {
     gameState: {
       ...newState.gameState,
       flyingObjects,
-      cannonBalls,
+      cannonBalls: [...cannonBalls],
+      lives,
+      started,
     },
     angle,
   };
